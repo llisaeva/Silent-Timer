b5ba0c7e13b734a679209290d6719caf4e0bde35
lisaeva <lis.lisaeva@gmail.com>
2021-03-25 22:11:11 -0500

    remove old file

--- app/build/generated/data_binding_trigger/debug/com/lisaeva/silenttimer/DataBindingTriggerClass.java
+++
@@ -1,4 +0,0 @@
-package com.lisaeva.silenttimer;
-
-@androidx.databinding.BindingBuildInfo
-public class DataBindingTriggerClass {}
--- app/src/main/java/com/lisaeva/silenttimer/ui/EditListFragment.java
+++ app/src/main/java/com/lisaeva/silenttimer/ui/EditListFragment.java
@@ -8,7 +8,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
-import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
